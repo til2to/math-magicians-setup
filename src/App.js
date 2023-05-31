adcf3892a1ef820d5584e03559541deb2f6887a4
til2to <padwusup@gmail.com>
2023-05-31 10:34:18 +0000

    Add quotes component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import DisplayQuotes from './components/Quotes/DisplayQuotes';
 import Calculator from './components/calculator/Calculator';
 
 const App = () => (
   <div className="App">
     <Calculator />
+    <DisplayQuotes />
   </div>
 );
 
